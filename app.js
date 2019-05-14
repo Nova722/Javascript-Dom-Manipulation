3ea000ec26b2964318281d0dfc0474a00e220609
Nova722 <42361014+Nova722@users.noreply.github.com>
2019-05-13 22:45:14 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,35 +4,99 @@ var tableData = data;
 // select tbody 
 tbody = d3.select("tbody")
 
-// using Object.entries to get key, value data inside of the table
-// and loop through them to add to the table in html
-function displayData(data){ 
+// loop through table using pbject entries
+function displayData(something){ 
     tbody.text("")
-    data.forEach(function(sighting){
+    something.forEach(function(et_sighting){
     new_tr = tbody.append("tr")
-    Object.entries(sighting).forEach(function([key, value]){
+    Object.entries(et_sighting).forEach(function([key, value]){
         new_td = new_tr.append("td").text(value)	
     })
 })}
 
 displayData(tableData)
 
-//select the web user's input and the filter button
-var dateInputText = d3.select("#datetime")
-var button = d3.select("filter-btn")
+//user input and filter button
+var dateInput = d3.select("#datetime")
+var button = d3.select("#filter-btn").on("click", clickSelect)
+
 
 // filter data with date that the user inputs
 function clickSelect(){
-    //don't refresh the page!
+    //no page refresh
     d3.event.preventDefault();
-    //print the value that was input
-    console.log(dateInputText.property("value"));
-    //create a new table showing only the filterd data
-    var new_table = tableData.filter(sighting => sighting.datetime===dateInputText.property("value"))
-    //display the new table
-    displayData(new_table);
+    console.log(dateInput.property("value"));
+    //create a new table with filter
+    var filter_table = tableData.filter(et_sighting => et_sighting.datetime===dateInput.property("value"))
+    //display new table
+    displayData(filter_table);
 }
 
-// event listener to handle change and click
-dateInputText.on("change", clickSelect)
+//user input and filter button
+var cityInput = d3.select("#city")
+var button2 = d3.select("#filter-btn2").on("click", clickSelectcity)
+
+// filter data with date that the user inputs
+function clickSelectcity(){
+    //no page refresh
+    d3.event.preventDefault();
+    console.log(cityInput.property("value"));
+    //create a new table with filter
+    var filter_table = tableData.filter(et_sighting => et_sighting.city===cityInput.property("value"))
+    //display the table
+    displayData(filter_table);
+}
+
+
+//user input and filter button
+var stateInput = d3.select("#state")
+var button3 = d3.select("#filter-btn3").on("click", clickSelectstate)
+
+// filter data with date that the user inputs
+function clickSelectstate(){
+    //no page refresh
+    d3.event.preventDefault();
+    console.log(stateInput.property("value"));
+    //create a new table with filter
+    var filter_table = tableData.filter(et_sighting => et_sighting.state===stateInput.property("value"))
+    //display the table
+    displayData(filter_table);
+
+}
+
+//user input and filter button
+var countryInput = d3.select("#country")
+var button4 = d3.select("#filter-btn4").on("click", clickSelectcountry)
+
+// filter data with date that the user inputs
+function clickSelectcountry(){
+    //no page refresh
+    d3.event.preventDefault();
+    console.log(countryInput.property("value"));
+    //create a new table with filter
+    var filter_table = tableData.filter(et_sighting => et_sighting.country===countryInput.property("value"))
+    //display the table
+    displayData(filter_table);
+}
+
+
+//user input and filter button
+var shapeInput = d3.select("#shape")
+//var shapeInput = shapeInput2.toLowerCase()
+var button5 = d3.select("#filter-btn5").on("click", clickSelectshape)
+
+// filter data with date that the user inputs
+function clickSelectshape(){
+    //no page refresh
+    d3.event.preventDefault();
+    console.log(shapeInput.property("value"));
+    //create a new table with filter
+    var filter_table = tableData.filter(et_sighting => et_sighting.shape===shapeInput.property("value"))
+    //display the table
+    displayData(filter_table);
+}
+
+
+
+
 
